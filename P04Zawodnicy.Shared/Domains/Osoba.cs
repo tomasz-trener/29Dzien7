9632fb121082396025373ed78076d645aa930b6c
kurs <kurs@DESKTOP-284UO99>
2024-11-15 12:08:02 +0100

    q

--- P04Zawodnicy.Shared/Domains/Osoba.cs
+++ P04Zawodnicy.Shared/Domains/Osoba.cs
@@ -7,26 +7,13 @@ using System.Threading.Tasks;
 
 namespace P04Zawodnicy.Shared.Domains
 {
-    public class Osoba : IComparable<Osoba>
+    public class Osoba 
     {
         public string Imie { get; set; }
         public string Nazwisko { get; set; }
 
-        public DateTime DataUrodzenia { get; set; }
+       
 
-        public int CompareTo(Osoba inna)
-        {
-            int pora1 = PoraRoku(this.DataUrodzenia);
-            int pora2 = PoraRoku(inna.DataUrodzenia);
-            return pora1.CompareTo(pora2);
-        }
-
-        private int PoraRoku(DateTime data)
-        {
-            if (data.Month >= 3 && data.Month <= 5) return 1; // wiosna
-            if (data.Month >= 6 && data.Month <= 8) return 2; // Lato
-            if (data.Month >= 9 && data.Month <= 11) return 3; // jesien
-            return 4;// zima
-        }
+      
     }
 }
--- P04Zawodnicy.Shared/Domains/Zawodnik.cs
+++ P04Zawodnicy.Shared/Domains/Zawodnik.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace P04Zawodnicy.Shared.Domains
 {
-    public class Zawodnik : Osoba
+    public class Zawodnik : Osoba, IComparable<Zawodnik>
     {
         public static string[] DostepneKolumny = { "Imie", "Nazwisko", "Kraj", "Waga", "Wzrost" };
 
@@ -64,6 +64,23 @@ namespace P04Zawodnicy.Shared.Domains
                     s += this[k] + " "; 
                 return s;
             }
+        }
+
+        public DateTime DataUrodzenia { get; set; }
+        public int CompareTo(Zawodnik inna)
+        {
+            int pora1 = PoraRoku(this.DataUrodzenia);
+            int pora2 = PoraRoku(inna.DataUrodzenia);
+            return pora1.CompareTo(pora2);
+        }
+
+
+        private int PoraRoku(DateTime data)
+        {
+            if (data.Month >= 3 && data.Month <= 5) return 1; // wiosna
+            if (data.Month >= 6 && data.Month <= 8) return 2; // Lato
+            if (data.Month >= 9 && data.Month <= 11) return 3; // jesien
+            return 4;// zima
         }
 
 
