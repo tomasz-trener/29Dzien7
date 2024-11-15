d4917f47648f3160170ebbb7b57225a27ae970be
kurs <kurs@DESKTOP-284UO99>
2024-11-15 11:24:26 +0100

    q

---
+++ P05Intrefejsy/Krzeslo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05Intrefejsy
+{
+    internal class Krzeslo : IComparable, ICloneable
+    {
+        public string Firma { get; set; }
+        public int Waga { get; set; }
+
+        public object Clone()
+        {
+            //return new Krzeslo()
+            //{
+            //    Firma = this.Firma.ToUpper(),
+            //    Waga = this.Waga
+            //};
+            return this.MemberwiseClone();
+        }
+
+        public int CompareTo(object obj)
+        {
+            Krzeslo inneKrzeslo = (Krzeslo)obj;
+            Krzeslo mojeKrzeslo = this;
+
+            //  return mojeKrzeslo.Firma.Length - inneKrzeslo.Firma.Length;
+            return string.Compare(mojeKrzeslo.Firma, inneKrzeslo.Firma); // alfabetycznie
+
+        }
+    }
+}
---
+++ P05Intrefejsy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05Intrefejsy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liczby = { 2, 5, 6, 3, 1 };
+
+            Array.Sort(liczby);
+
+            Console.WriteLine(string.Join(" ",liczby));
+        
+
+            Krzeslo[] krzesla = new Krzeslo[] 
+            {
+                new Krzeslo {Firma="zzzz",Waga=2},
+                new Krzeslo {Firma="xxx",Waga=3},
+                new Krzeslo {Firma="yyyyy",Waga=1},
+            };
+
+            Array.Sort(krzesla);
+
+            foreach (var k in krzesla)
+            {
+                Console.WriteLine(k.Firma  + " " + k.Waga);
+            }
+
+            // inne intrefejsy np: IClonable 
+
+            Krzeslo k1 = new Krzeslo() { Firma = "xyz", Waga = 30 };
+            Krzeslo k2 = k1; // przekaznie przez refencje 
+
+            k1.Waga++; // zmieni obiekt na ktory wskazuje k1 i k2 
+
+            Krzeslo k3 = new Krzeslo() // teraz mam kopie 
+            {
+                Firma = k1.Firma,
+                Waga = k1.Waga,
+            };
+
+            Krzeslo k4 = (Krzeslo)k1.Clone(); // teraz mam kopie
+
+
+            Console.ReadKey();
+        }
+    }
+}
