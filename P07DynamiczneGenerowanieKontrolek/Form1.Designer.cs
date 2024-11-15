7c88b646552da15b239d0ce062e84fcd59b84bb9
kurs <kurs@DESKTOP-284UO99>
2024-11-15 14:28:23 +0100

    q

---
+++ P07DynamiczneGenerowanieKontrolek/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace P07DynamiczneGenerowanieKontrolek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnWczytaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnWczytaj
+            // 
+            this.btnWczytaj.Location = new System.Drawing.Point(12, 56);
+            this.btnWczytaj.Name = "btnWczytaj";
+            this.btnWczytaj.Size = new System.Drawing.Size(75, 23);
+            this.btnWczytaj.TabIndex = 2;
+            this.btnWczytaj.Text = "Wczytaj";
+            this.btnWczytaj.UseVisualStyleBackColor = true;
+            this.btnWczytaj.Click += new System.EventHandler(this.btnWczytaj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 595);
+            this.Controls.Add(this.btnWczytaj);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnWczytaj;
+    }
+}
+
---
+++ P07DynamiczneGenerowanieKontrolek/Form1.cs
@@ -0,0 +1,74 @@
+﻿using P04Zawodnicy.Shared.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P07DynamiczneGenerowanieKontrolek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = new Button();
+            b.Text = "OK";
+            b.Location = new Point(100, 100);
+            b.Click += przyciskDynamiczny_Click;
+            b.Name = "btnDynamiczny";
+            this.Controls.Add(b);
+
+
+            foreach (Control item in Controls)
+            {
+                if (item is Button & ((Button)item).Name == "btnDynamiczny")
+                    item.BackColor = Color.Red;
+            }
+        }
+
+        private void przyciskDynamiczny_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(((Button)sender).Text);
+        }
+
+        private void btnWczytaj_Click(object sender, EventArgs e)
+        {
+            IManagerZawodnikow mz = new ManagerZawodnikow();
+            var zawodnicy = mz.WczytajZawodnikow();
+
+            int x = 200;
+            int y = 10;
+            int k = 0;
+
+            foreach (var z in zawodnicy)
+            {
+                Button b = new Button();
+                b.Text = z.ImieNazwisko;    
+                b.Location= new Point(x, y);
+                b.Click += przyciskDynamiczny_Click;
+                Controls.Add(b);
+                b.Width = 50;
+                b.Height = 50;
+                x += 60;
+                k++;
+                if (k == 4)
+                {
+                    k = 0;
+                    x = 200;
+                    y += 60;
+                }
+            }
+        }
+
+      
+    }
+}
